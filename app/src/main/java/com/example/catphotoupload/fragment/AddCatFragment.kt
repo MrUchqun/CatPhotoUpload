9232cf3834f91107feffed52f3ea0ca9f33829c6
Uchqun Azimboyev <79976346+MrUchqun@users.noreply.github.com>
2022-03-16 17:03:10 +0500

    initial comment

--- app/src/main/java/com/example/catphotoupload/fragment/AddCatFragment.kt
+++ app/src/main/java/com/example/catphotoupload/fragment/AddCatFragment.kt
@@ -1,31 +1,24 @@
 package com.example.catphotoupload.fragment
 
-import android.R.attr.path
+import android.annotation.SuppressLint
 import android.app.Activity.RESULT_OK
 import android.content.Intent
-import android.database.Cursor
+import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
-import android.os.Parcelable
 import android.provider.MediaStore
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.GONE
+import android.view.View.*
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
 import com.example.catphotoupload.R
 import com.example.catphotoupload.networking.RetrofitHttp
-import com.kusu.loadingbutton.LoadingButton
-import com.sangcomz.fishbun.FishBun
-import com.sangcomz.fishbun.FishBun.Companion.INTENT_PATH
-import com.sangcomz.fishbun.adapter.image.impl.GlideAdapter
-import okhttp3.MediaType
-import okhttp3.MultipartBody
-import okhttp3.RequestBody
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
@@ -44,7 +37,9 @@ class AddCatFragment : Fragment() {
 
     private lateinit var ivPhoto: ImageView
     private lateinit var llPreview: LinearLayout
-    private lateinit var btnUpload: LoadingButton
+    private lateinit var btnUpload: Button
+    private lateinit var progressBar: ProgressBar
+    private val pickImage = 100
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -63,7 +58,7 @@ class AddCatFragment : Fragment() {
         ivPhoto = view.findViewById(R.id.iv_photo)
         llPreview = view.findViewById(R.id.ll_preview)
         btnUpload = view.findViewById(R.id.btn_upload)
-
+        progressBar = view.findViewById(R.id.progressBar)
 
         btnUpload.setOnClickListener {
             val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
@@ -71,33 +66,17 @@ class AddCatFragment : Fragment() {
         }
     }
 
-    private val pickImage = 100
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == RESULT_OK && requestCode == pickImage) {
 
-            val file = getFile(data?.data!!)
-            val reqFile: RequestBody = RequestBody.create(MediaType.parse("image/jpg"), file)
-            val body: MultipartBody.Part = MultipartBody.Part.createFormData("file", file.name, reqFile)
+            llPreview.visibility = GONE
+            ivPhoto.setImageURI(data?.data)
+            btnUpload.setText(R.string.txt_upload)
 
-            val responseBodyCall: Call<ResponseBody> = RetrofitHttp.photoService.uploadFile(body, file.name)
-
-            responseBodyCall.enqueue(object : Callback<ResponseBody> {
-                override fun onResponse(
-                    call: Call<ResponseBody>,
-                    response: Response<ResponseBody>
-                ) {
-                    Log.d("Success", "success " + response.code());
-                    Log.d("Success", "success " + response.message());
-                }
-
-                override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
-                    Log.e("failure", "message = " + t.message);
-                    Log.e("failure", "cause = " + t.cause);
-                }
-
-            })
+            btnUpload.setOnClickListener {
+                apiUploadImage(getFile(data?.data!!))
+            }
         }
     }
 
@@ -115,4 +94,38 @@ class AddCatFragment : Fragment() {
         return file
     }
 
+    private fun apiUploadImage(file: File) {
+        showLoading()
+        val responseBodyCall: Call<ResponseBody> =
+            RetrofitHttp.photoService.uploadFile(
+                RetrofitHttp.getMultipartBody("file", file),
+                file.name
+            )
+
+        responseBodyCall.enqueue(object : Callback<ResponseBody> {
+            override fun onResponse(
+                call: Call<ResponseBody>,
+                response: Response<ResponseBody>
+            ) {
+                hideLoading(R.string.txt_uploaded)
+                btnUpload.setTextColor(Color.GREEN)
+            }
+
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                hideLoading(R.string.txt_uploading_fail)
+                btnUpload.setTextColor(Color.RED)
+            }
+
+        })
+    }
+
+    private fun showLoading() {
+        btnUpload.text = ""
+        progressBar.visibility = VISIBLE
+    }
+
+    private fun hideLoading(text: Int) {
+        btnUpload.setText(text)
+        progressBar.visibility = INVISIBLE
+    }
 }
--- app/src/main/java/com/example/catphotoupload/networking/RetrofitHttp.kt
+++ app/src/main/java/com/example/catphotoupload/networking/RetrofitHttp.kt
@@ -1,9 +1,12 @@
 package com.example.catphotoupload.networking
 
 import com.example.catphotoupload.networking.service.CatPhotoService
-import okhttp3.OkHttpClient
+import okhttp3.MediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
 
 object RetrofitHttp {
     private const val IS_TESTER = true
@@ -11,7 +14,9 @@ object RetrofitHttp {
     private const val SERVER_PRODUCTION = "https://api.thecatapi.com/v1/"
 
     private val retrofit: Retrofit =
-        Retrofit.Builder().baseUrl(server()).addConverterFactory(GsonConverterFactory.create())
+        Retrofit.Builder()
+            .baseUrl(server())
+            .addConverterFactory(GsonConverterFactory.create())
             .build()
 
     private fun server(): String {
@@ -21,4 +26,11 @@ object RetrofitHttp {
     }
 
     val photoService: CatPhotoService = retrofit.create(CatPhotoService::class.java)
+
+    fun getMultipartBody(key: String, file: File): MultipartBody.Part {
+        val reqFile: RequestBody = RequestBody.create(MediaType.parse("image/jpg"), file)
+        return MultipartBody.Part.createFormData(key, file.name, reqFile)
+    }
+
+
 }
